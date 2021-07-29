6a9e3ae33d5a5c21b394ae71c1151209180ad5fd
sinanoral <sinnanoral@gmail.com>
2021-07-29 16:13:32 +0300

    disable fluentvalidation localization

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -22,6 +23,7 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ValidatorOptions.Global.LanguageManager.Enabled = false;
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
